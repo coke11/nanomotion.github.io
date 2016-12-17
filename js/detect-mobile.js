dbb68dcc2c2fd70c29ccf90b3113e461f6d133d6
Nanomotion <Nanomotion@users.noreply.github.com>
2016-12-17 17:16:58 -0500

    Create detect-mobile.js

---
+++ js/detect-mobile.js
@@ -0,0 +1,28 @@
+function detectmobile() { 
+ if( navigator.userAgent.match(/Android/i)
+ || navigator.userAgent.match(/webOS/i)
+ || navigator.userAgent.match(/iPhone/i)
+ || navigator.userAgent.match(/iPad/i)
+ || navigator.userAgent.match(/iPod/i)
+ || navigator.userAgent.match(/BlackBerry/i)
+ || navigator.userAgent.match(/Windows Phone/i)
+ ){
+    return true;
+  }
+ else {
+    return false;
+  }
+}
+
+function detectmobile2() {
+   if(window.innerWidth <= 1080 && window.innerHeight <= 1920) {
+     return true;
+   } else {
+     return false;
+   }
+}
+
+if detectmobile() && detectmobile2() {
+  console.info("User is on a mobile device; switching to mobile view")
+  document.location = "https://nanomotion.github.io/mobile"
+}
