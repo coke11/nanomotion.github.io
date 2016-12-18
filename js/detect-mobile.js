38f9505889904a46671c61c2233b3ae56d86fb36
Nanomotion <Nanomotion@users.noreply.github.com>
2016-12-17 19:59:10 -0500

    Update detect-mobile.js

--- js/detect-mobile.js
+++ js/detect-mobile.js
@@ -23,7 +23,9 @@ var detectmobile2 = function() {
 }
 if(!(document.location == "https://nanomotion.github.io/#ignore-mobile")) {
  if(detectmobile() && detectmobile2()) {
-   console.info("User is on a mobile device; switching to mobile view")
+   console.info("User is on a mobile device; switching to mobile view");
    document.location = "https://nanomotion.github.io/mobile"
  }
+} else {
+ console.info("document.location = " + document.location);
 }
