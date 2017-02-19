7b36754179b516fbe6557eb0ded345af4eff020d
ナノ <Nanomotion@users.noreply.github.com>
2017-02-19 12:27:19 -0500

    Update redirect.js

--- js/redirect.js
+++ js/redirect.js
@@ -1,3 +1,12 @@
 /* For redirects such as maintenance */
 
-window.href.replace("https://git.io/vD5o4");
+if (window.console) {
+  window.console.error("The page responded with 503")
+}
+
+document.location = "https://git.io/vD5o4"
+
+/*
+if (!(document.location.hostname == "nanomotion.github.io)) {
+  document.location = "https://nanomotion.github.io" + document.location.pathname
+}*/
