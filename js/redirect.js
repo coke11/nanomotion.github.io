7203e2bcb51e1dc4cd3e829fcbb949a437ec835d
ナノ <Nanomotion@users.noreply.github.com>
2017-02-20 10:49:33 -0500

    Update redirect.js

--- js/redirect.js
+++ js/redirect.js
@@ -1,14 +1,14 @@
 if (!(document.location.hostname == "nanomotion.github.io")) {
-  window.open("https://nanomotion.github.io" + document.location.pathname)
+  window.open("https://nanomotion.github.io" + document.location.pathname);
   window.close()
 }
 
 /* For redirects such as maintenance */
 
 if (window.console) {
-  window.console.error("Nanomotion.github.io is unavailable")
+  window.console.error("Nanomotion.github.io is unavailable");
 }
 
-if (!(document.location.pathname == "/") {
+if (!(document.location.pathname == "/")) {
   document.location = "https://nanomotion.github.io/"
 }
